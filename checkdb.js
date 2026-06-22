290e256d5ce3e161abc67e876fb3b36ef92c0d26
hung-sang <fatking474@gmail.com>
2026-06-22 21:26:27 +0700

    ghi chú nội dung vừa sửa

--- checkdb.js
+++
@@ -1,5 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('data/database.sqlite');
-db.all("SELECT * FROM declarations ORDER BY createdAt DESC LIMIT 2", (err, rows) => {
-    console.log(JSON.stringify(rows, null, 2));
-});
